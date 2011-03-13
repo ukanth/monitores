3bc765ea6d4509cdd921865804cd491163a72472
cumakt@gmail.com <cumakt@gmail.com@69527746-e490-11dd-ab07-b19b4dad1b3d>
2011-03-13 07:51:49 +0000

    missed resource.h

---
+++ 1.0.1/resource.h
@@ -0,0 +1,29 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define TRAY_ICON                               91
+#define IDD_SETUP_DIALOG                        101
+#define IDC_COMBO1                              1000
+#define IDC_MONITOR_STATUS                      1002
+#define IDC_YAHOO                               1028
+#define IDC_CUSTOM_MSG                          1029
+#define IDC_SET_AWAY                            1032
+#define IDC_MONITOR                             1033
+#define IDC_SCREEN                              1037
+#define IDC_DISABLE_SCREEN                      1039
+#define IDC_PAUSE_MEDIA                         1040
+#define IDC_MUTE_VOL                            1042
+#define IDC_YAHOO_EDIT                          1043
+#define IDC_MSG_EDIT                            1044
+#define IDC_MHOTKEY                             1045
+#define IDC_SHOTKEY                             1046
+#define IDC_ONEHOTKEY                           1047
+#define IDC_START_WIN                           1052
+#define IDC_SAVE                                1054
+#define IDC_CANCEL                              1055
+#define IDC_MUHOTKEY                            1057
+#define IDC_HP_KEY                              1061
+#define IDC_PREF_HKEY                           1063
+#define IDC_PREFLABEL                           1064
+#define IDC_HIDE_TRAY                           1065
